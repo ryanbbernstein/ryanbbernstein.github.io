aaaeb1af4d655d9bcfc1218444524139b001240e
Ryan Bernstein <ryanbbernstein@gmail.com>
2020-03-04 14:19:25 -0800

    modifying modern-resume-theme to allow for images next to an entry. only done for left.html but will add to all others later. also added logic for modal popup when clicking on the image.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+
+var modal = document.getElementById("imgModal");
+var modalImg = document.getElementById("img01");
+var captionTxt = document.getElementById("caption");
+
+
+function OpenModal(img) {
+	modalImg.src = img.src;
+	captionTxt.innerHTML = img.alt;
+	modal.style.display = "block";
+}
+
+document.getElementsByClassName("closeModal")[0].onclick = function() {
+	modal.style.display = "none";
+}
